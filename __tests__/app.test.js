1d97f59975276ecbfc28c26584f86c53cd8b5fea
Pedro <pedro.chn01@gmail.com>
2022-08-10 16:27:54 -0700

    added test and started controller

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,14 +1,27 @@
 const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
+const { request } = require('express');
 // const request = require('supertest');
 // const app = require('../lib/app');
 
-describe('backend-express-template routes', () => {
+describe('i-auth routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+  it('should login and redirect users to /api/v1/github/dashboard', async () => {
+    const res = await request
+      .agent(app)
+      .get('/api/v1/github/vista?code=42')
+      .redirects(1);
+    
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      username: 'I_am_not_real',
+      email: 'fake@fake.com',
+      avatar: expect.any(String),
+      iat: expect.any(Number),
+      exp: expect.any(Number),
+    })
   });
   afterAll(() => {
     pool.end();
