c24e6948f2f499079c0256cebd22fd10ff68039a
Pedro <pedro.chn01@gmail.com>
2022-08-10 17:04:59 -0700

    adding models

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,8 +1,8 @@
 const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
-const { request } = require('express');
-// const request = require('supertest');
-// const app = require('../lib/app');
+//const { request } = require('express');
+const request = require('supertest');
+const app = require('../lib/app');
 
 describe('i-auth routes', () => {
   beforeEach(() => {
